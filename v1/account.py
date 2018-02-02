7b3d00013bf857cb5892eeebece18d178ab34954
Zer0Name <aap220@scarletmail.rutgers.edu>
2018-02-02 12:43:42 -0500

    added activate account

--- v1/account.py
+++ v1/account.py
@@ -39,7 +39,9 @@ def index():
 	if not user_table.insert(new_user.dict()):
 		return ERROR(1,"Email address already in use")
 
-	return jsonify(	{"Success" : True , "Description" : "Account created"})  
+	return jsonify(	{"Success" : True , "Description" : "Account created"}) 
+
+
 
 @accnt.route('/send-access-code', methods=['POST'])
 def send_access_code():
@@ -65,20 +67,23 @@ def send_access_code():
 	lower = 10**(6-1)
 	upper = 10**6 - 1
 	num = str(random.randint(lower, upper))
-	#connect to server and send message to number
+
+	#create key and get user
 	key = {}
 	key["Email"] = str(content["Email"])
 	response = user_table.get(key)
 	if response == None:
 		return  ERROR(-1,"Error with info")
-	#check if account is activated already
-	if response["Activated"] == True:
-		return ERROR(-1,"Error, account already activated")
 
 	#create user object with current user info 
 	current_user = user.user(str(response["Username"]),str(response["Password"]),str(response["Email"].lower()),
-							 str(response["Phone_number"]), str(response["Location"]),str(response["Activated"]), 
+							 str(response["Phone_number"]),str(response["Activated"]), 
 							 str(response["Id"] ))
+
+	#check if account is activated already
+	if current_user.get_activated() == "True":
+		return ERROR(-1,"Error, account already activated")
+
 
 	#update user phone number
 	current_user.set_phone_number(num)
@@ -91,6 +96,75 @@ def send_access_code():
 	sns.publish(PhoneNumber = number, Message=num)
 	return jsonify(	{"Success" : True , "Description" : "Code sent to number"}) 
 
+@accnt.route('/activate-account', methods=['POST'])
+def activate_account():
+	# check to see if request is in json form 
+	if not request.is_json:
+		return ERROR(-1,"Request was not in the form of a json")
+	# store info in memory
+	content = request.get_json() 
+
+	# check to see that only 2 inputs are given 
+	if len(content) != 3:
+		return ERROR(-1,"To many or to little argument given")
+
+	if content.get('Email') == None or content.get('Phone_number') == None  or content.get('Access_code') == None :
+		return ERROR(-1,"Dont have correct argument present")
+
+	#connect to table 
+	user_table = usr_table.Users_table("users","Email")
+
+	#create key and get user
+	key = {}
+	key["Email"] = str(content["Email"])
+	response = user_table.get(key)
+	if response == None:
+		return  ERROR(-1,"Error with info")
+
+	#create user object with current user info 
+	current_user = user.user(str(response["Username"]),str(response["Password"]),str(response["Email"].lower()),
+							 str(response["Phone_number"]),str(response["Activated"]), 
+							 str(response["Id"] ))
+
+	#check if account is activated already
+	if current_user.get_activated() == "True":
+		return ERROR(-1,"Error, account already activated")
+
+	if current_user.get_phone_number() == "None":
+		return ERROR(-1,"Error, no access code available for user")
+
+	if not current_user.get_phone_number() == str(content["Access_code"]):
+		return ERROR(1,"Error, issue with Access code")
+
+	#set new number to account 
+	current_user.set_phone_number(str(content["Phone_number"]))
+
+	#set acctivated to true
+	current_user.set_activated("True")
+
+	#update database
+	new_data,old_data = current_user.generate_change()
+	if not user_table.update(key,new_data,old_data):
+		return ERROR(-1, "Error updating database")
+
+	return jsonify(	{"Success" : True , "Description" : "Account activated and phone number confirmed"})
+
+
+
+
+@accnt.route('/login', methods=['POST'])
+def login():
+	if not request.is_json:
+		return ERROR(-1,"Request was not in the form of a json")
+	# store info in memory
+	content = request.get_json() 
+
+	# check to see that only 2 inputs are given 
+	if len(content) != 2:
+		return ERROR(-1,"To many or to little argument given")
+
+	if content.get('Email') == None or content.get('Password') == None:
+		return ERROR(-1,"Dont have correct argument present")
 
 
 
